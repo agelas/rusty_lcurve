42f93d2f89524cddb9d48b985a8b406b98f02993
Mathias <mathiaswi177@gmail.com>
2024-10-08 01:44:27 -0700

    Update main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod db;
 mod tui;
 mod utils;
 
-fn main() {
-    println!("Hello, world!");
+use crate::tui::tui::App;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let rusty_lcurve_tui = App::start_ui();
+    Ok(())
 }
