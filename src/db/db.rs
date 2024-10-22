547be3d782bb0e36fbc2cdfc2e4727f1cb6f2ebb
Mathias <mathiaswi177@gmail.com>
2024-10-22 16:11:24 -0700

    Add fns for seeing if problem already exists and for insertion into sqlite

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,5 +1,5 @@
 use crate::db::models::LCProblem;
-use rusqlite::{Connection, Result};
+use rusqlite::{params, Connection, Result};
 use std::fs;
 
 pub fn init_db(db_path: &str) -> Result<()> {
@@ -30,6 +30,37 @@ pub fn get_all_problems(conn: &Connection) -> Result<Vec<LCProblem>> {
         })?
         .collect::<Result<Vec<_>, _>>()?;
     Ok(problems)
+}
+
+pub fn problem_exists(conn: &Connection, lc_number: u32, problem_name: &str) -> Result<bool> {
+    let mut stmt =
+        conn.prepare("SELECT COUNT(1) FROM problems WHERE lc_number = ?1 OR problem_name = ?2")?;
+
+    let count: u32 = stmt.query_row(params![lc_number, problem_name], |row| row.get(0))?;
+    Ok(count > 0)
+}
+
+// The necessity of having an LCProblem struct is a bit questionable
+pub fn insert_problem(
+    conn: &Connection,
+    lc_number: u32,
+    problem_name: &str,
+    problem_type: &str,
+) -> Result<()> {
+    let lc_problem = LCProblem::new(lc_number, problem_name, problem_type);
+    conn.execute(
+        "INSERT INTO problems (id, lc_number, problem_name, problem_type, start_date, last_practiced, times_practiced) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+        params![
+            lc_problem.id,
+            lc_problem.lc_number,
+            lc_problem.problem_name,
+            lc_problem.problem_type,
+            lc_problem.start_date.to_string(),
+            lc_problem.last_practiced.to_string(),
+            lc_problem.times_practiced,
+        ]
+    )?;
+    Ok(())
 }
 
 fn create_table(conn: &Connection) -> Result<()> {
