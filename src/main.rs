8a177e8e76b233b35655e079686cbb3c3b48b59f
Mathias <mathiaswi177@gmail.com>
2024-10-22 16:54:10 -0700

    DB setup and update app initialization logic in main

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ mod db;
 mod tui;
 mod utils;
 
+use db::db::{get_connection, init_db};
+
 use crate::tui::tui::App;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let rusty_lcurve_tui = App::start_ui();
+    let db_path = "rusty_l_db";
+    if let Err(err) = init_db(db_path) {
+        println!("Error initializing database: {:?}", err);
+    }
+
+    let db_connection = get_connection(db_path).unwrap();
+    let rusty_lcurve_tui = App::start_ui(db_connection);
     Ok(())
 }
